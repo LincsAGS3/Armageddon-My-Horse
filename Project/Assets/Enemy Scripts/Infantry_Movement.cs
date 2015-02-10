e478e2d2d8d151b1877882d6198953001edaba09
Adam Bowes <skylinegames@hotmail.co.uk>
2015-02-10 21:45:31 +0000

    collision detection added and group point sorting started

--- Project/Assets/Enemy Scripts/Infantry_Movement.cs
+++ Project/Assets/Enemy Scripts/Infantry_Movement.cs
@@ -5,26 +5,26 @@ using System;
 public class Infantry_Movement : MonoBehaviour {
 
 	public GameObject groupPoint;
-	float maxSpeed = 4;
+	public float maxSpeed = 4;
 	float currentSpeed = 0;
 	Vector2 speed = new Vector2(0,0) ;
-	float wanderRadius = 15;
-	Vector2 gotoPoint = new Vector2(-1,-1);
+	public float wanderRadius = 15;
+	Vector2 gotoPoint = new Vector2();
 	void Start () {
-	
+		gotoPoint = this.transform.position;
 	}
 	
 
 	void Update () {
-		//new point if the old one is reached or the point is invalid
-		if ((gotoPoint.x == -1 && gotoPoint.y == -1 )|| Vector2.Distance(gotoPoint,this.transform.position)<1) {
+		//new point if the old one is reached 
+		if ( Vector2.Distance(gotoPoint,this.transform.position)<1) {
 			gotoPoint = FindNextPoint (groupPoint.transform.position);
-			Debug.Log (gotoPoint);//output new target for bug testing
+			//Debug.Log (gotoPoint);//output new target for bug testing
 		}
 		//move the character
 		move ();
 	}
-	Vector2 FindNextPoint(Vector2 centre)
+	Vector2 FindNextPoint(Vector2 centre)//calculate a point around the centre within a 15 unit radius
 	{
 		//create a random vector 2 with a magnitude of 1
 		Vector2 rnd = UnityEngine.Random.insideUnitCircle;
@@ -34,7 +34,7 @@ public class Infantry_Movement : MonoBehaviour {
 		return centre+rnd;
 
 	}
-	void move()
+	void move()//smoothly move towards gotoPoint
 	{
 		//if speed isnt at the max then increase the speed
 		if (currentSpeed < maxSpeed) {
@@ -47,5 +47,15 @@ public class Infantry_Movement : MonoBehaviour {
 		speed.y += -(speed.y-(-(float)Math.Sin (rotation) * currentSpeed*Time.deltaTime))*0.1f;
 		//apply the movement
 		this.transform.Translate (speed);
+	}
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log ("collision");
+		//if we are colliding with another enemy
+		if (coll.gameObject.tag == this.tag) {
+			//move away from the enemy
+			float rotation = (float)Math.Atan2 (this.transform.position.y - coll.transform.position.y,this.transform.position.x - coll.transform.position.x);
+			Vector2 moveAway = new Vector2((-(float)Math.Cos(rotation)),(-(float)Math.Sin(rotation)));
+			this.transform.Translate(moveAway*0.1f);
+		}
 	}
 }
---
+++ Project/Assets/GroupPoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GroupPoint : MonoBehaviour {
+
+	public bool vacancy = true;
+	public int maxSize = 16;
+	public int size=0;
+	public List<GameObject> enemies = new List<GameObject>();
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		size = enemies.Count;
+	}
+}
