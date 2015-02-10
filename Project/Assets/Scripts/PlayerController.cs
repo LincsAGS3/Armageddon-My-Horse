e945a0f1f3b5eadad476db92a77060ed89d5fe73
SH4PPY <shapcottman@hotmail.com>
2015-02-10 19:10:04 +0000

    Player Movement
    Added place holder for enemy, obstacles and player.
    Given player float value for health that decreases on contact with
    enemy.
    Player moves as requested, always moving forwards, turning (through
    rotation) left and right, with the speed of the character being altered
    through key-presses (Arrow keys and WASD)

---
+++ Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	//player controller
+	//player speed
+	public float speed = 5;
+	//player rotate speed
+	public float rotateSpeed = 50;
+	//Player Health
+	public float health = 10;
+	//player movement
+	void Update ()
+	{	
+		//takes input from A,D LeftArrow AND RightArrow
+		transform.Rotate (0, 0, -Input.GetAxis ("Horizontal") * rotateSpeed * Time.deltaTime);
+		//Takes input from W, S, UpArrow and DownArrow
+		speed += Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+		//Sets minimum and maximum speed values
+		speed = Mathf.Clamp (speed, 1, 10);
+	}
+	void FixedUpdate()
+	{
+		rigidbody.velocity = transform.up * speed;
+	}
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.tag == "Enemy")
+		{
+			//Function can be changed to remove health decrementally, orm in this case, "insta-kill"
+			health--;
+		}
+	}
+}
