f199a794c06eba5c12a86fa5dce505d524368770
Core1019 <will.cotterill@btinternet.com>
2015-02-12 12:36:08 +0000

    Map etc added
    +Map
    +Water
    +Placeholder trees
    +Rocks
    +Placeholder forts
    +Placeholder castle & moat
    +Tags
    +Collisions
    +Bridges
    +Placeholder pause menu
    +Scenes orders set in build settings
    +Placeholder game over screen
    +Placeholder victory screen

---
+++ Project/Assets/Scripts/EndGame.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+///Have a different script change this to 1 or 2 when the player wins or dies, respectively
+var endGame = 0;
+
+function Start () {
+
+}
+
+function Update () {
+////////Testing whether the scene changes work
+	if (Input.GetKey("1"))
+	{
+		endGame = 1;
+	}
+
+	if (Input.GetKey("2"))
+	{
+		endGame = 2;
+	}
+
+///Load the victory or game over screens
+	if (endGame == 1)
+	{
+		Application.LoadLevel(2); //Victory
+	}
+
+	if (endGame == 2)
+	{
+		Application.LoadLevel(3); //Game Over
+	}
+
+}
---
+++ Project/Assets/Scripts/Pause.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+if (Input.GetKey("p"))
+{
+	Time.timeScale = 0;
+	var script3 = GetComponent(PauseMenuScript);
+	script3.enabled = true;
+}
+
+}
