d0b1b20977b0b8e9717f308bbad2f834a9ddbd51
Thomas-Fisher <thomas-fisher@live.co.uk>
2015-02-18 22:22:05 +0000

    Victory and Defeat screens created
    Victory and Defeat screens implemented, also created a screen state
    control for the main game for the menus which are displayed
    
    Scripts Created :
    MenuGameMenuStateScript
    MenuVictoryScript
    MenuDefeatScript
    
    1 line deleted and 1 line added to MenuInputThreeInitals Script
    
    Recreated the menu prefabs due to some small changes to the main menu
    prefab ( mainly adding background placeholders and making the UI scale
    with screen size)

---
+++ Project/Assets/Menu Scripts/MenuGameMenuStateScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuGameMenuStateScript : MonoBehaviour {
+											//0 No menu
+	static GameObject VictoryMenu;			//1 = victory
+	static GameObject DefeatMenu;			//2 = Defeat
+	static GameObject ThreeCharInputMenu;	//3 = ThreeCharInput
+	static GameObject LeaderboardMenu;		//4 = Leaderboard
+	
+	// Use this for initialization
+	void Start ()
+	{
+		//finds the menu holder objects
+		VictoryMenu = GameObject.Find ("VictoryMenu"); 
+		DefeatMenu = GameObject.Find ("DefeatMenu");
+		ThreeCharInputMenu = GameObject.Find ("NameInputMenu");
+		LeaderboardMenu = GameObject.Find ("LeaderboardMenu");
+		MenuStateChange (0); // game starts  with showing nothing
+	}
+	
+	public static void MenuStateChange (int MenuState)
+	{
+		switch (MenuState) {
+		case 0: //hide everything
+			VictoryMenu.SetActive(false); 
+			DefeatMenu.SetActive(false); 
+			ThreeCharInputMenu.SetActive(false); 
+			LeaderboardMenu.SetActive(false); 
+			break;
+		case 1: 
+			VictoryMenu.SetActive (true); 
+			break;
+		case 2: 
+			DefeatMenu.SetActive (true); 
+			break;
+		case 3: 
+			Debug.Log("entred");
+			VictoryMenu.SetActive (false); 
+			ThreeCharInputMenu.SetActive (true); 
+			break;
+		case 4: 
+			ThreeCharInputMenu.SetActive (false);
+			LeaderboardMenu.SetActive (true); 
+			break;
+		}
+	}
+}
