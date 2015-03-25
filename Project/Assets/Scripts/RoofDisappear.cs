a6b0c94ff56d43405cf13240787c549a2e9b56f6
DeathLord00 <a.deathridge@btinternet.com>
2015-03-25 19:31:23 +0000

    Both fort roofs disappear

---
+++ Project/Assets/Scripts/RoofDisappear.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoofDisappear : MonoBehaviour {
+	public GameObject roof;
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject.tag == "Player") 
+		{
+			roof.GetComponent<SpriteRenderer>().color = new Color (1f,1f,1f,0.1f);
+			print ("Player in box");
+		}
+	}
+}
